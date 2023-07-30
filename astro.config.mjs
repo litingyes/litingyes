46af42234c8d05c8f2033b1f13c38e75c6ddef74
liting <luz.liting@gmail.com>
2023-07-31 00:38:21 +0800

    chore(dep): add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config'
 import UnoCSS from 'unocss/astro'
+import sitemap from '@astrojs/sitemap'
 
 export default defineConfig({
+  site: 'https://liting.ink',
   integrations: [
     UnoCSS({
       injectReset: true,
     }),
+    sitemap(),
   ],
 })
